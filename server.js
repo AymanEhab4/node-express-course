9b298a9dcbfadffcb7987a97ea7e4eaf5af95b5c
unknown <aymanehab660@gmail.com>
2022-04-04 13:56:48 +0200

    first get route

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, () => {
+const usersData = [
+	{name: "Ayman"},
+	{name: "Mark"}
+]
+app.get('./users', (req, res) => {
+	res.json({
+		success: true,
+		message: "Successfully Got users, Nice",
+		users: usersData
+	})
+})
+
+
+
+app.listen(8000, () => {
 	console.log("Server is Running.")
 })
